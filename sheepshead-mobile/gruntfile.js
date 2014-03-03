df768cc815ebb487502911a42941b2d7d2ac16b2
Greg Smith <smr.kyu@gmail.com>
2014-03-03 13:54:47 -0600

    Setting noImplicitAny

--- sheepshead-mobile/gruntfile.js
+++ sheepshead-mobile/gruntfile.js
@@ -14,7 +14,8 @@ module.exports = function (grunt) {
 				target: "es5",
 				module: "commonjs",
 				sourcemap: true,
-				ignoreTypeCheck: false
+				ignoreTypeCheck: false,
+				noImplicitAny: true
 			},
 			scripts: {
 				src: ["src/scripts/**/*.ts"],
